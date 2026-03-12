f1237f9ba061abe92918bdccc27b83aea80a75c1
Stephenson-Lab <m.stephenson1@uea.ac.uk>
2026-03-12 13:22:42 +0000

    Update tasks.js

--- modules/CHE5150B/tasks.js
+++ modules/CHE5150B/tasks.js
@@ -32,5 +32,13 @@ You may attempt the activity multiple times. This tool is intended for formative
   7: { title: "CHE-5150B - Q6", formula: "This field is defunct - now generated from smiles",  requireStereo: false, prompt: "Draw the structure of the missing product", answersSmiles: ["O=C(O)[C@@H](NC(C)=O)[C@@H]1N=C(C(O)=O)C(CS1)=C"] },
   8: { title: "CHE-5150B - Q7", formula: "This field is defunct - now generated from smiles",  requireStereo: false, prompt: "Draw the structure of the missing intermediate - use F to represent R!", answersSmiles: ["O=C(O)[C@@H](/C(O1)=C/CO)N[C@H]1CC(OCF)=O"] },
   9: { title: "CHE-5150B - Q8", formula: "This field is defunct - now generated from smiles",  requireStereo: false, prompt: "Draw the full structure including your placement of the additional methyl group", answersSmiles: ["Cc3ccc(NC(=O)c2ccc(CN1CCN(C)CC1)cc2)cc3Nc5nccc(c4cccnc4)n5"] },
-  10: { title: "CHE-5150B - Q9", formula: "This field is defunct - now generated from smiles",  requireStereo: false, prompt: "Edit", answersSmiles: [""] },
+  10: { title: "CHE-5150B - Q10", formula: "This field is defunct - now generated from smiles",  requireStereo: false, prompt: "Edit", answersSmiles: [""] },
+  11: { title: "CHE-5150B - Q11", formula: "This field is defunct - now generated from smiles",  requireStereo: false, prompt: "Edit", answersSmiles: [""] },
+  12: {
+  title: "Test Q",
+  formula: "C29H31N7O",
+  requireStereo: false,
+  prompt: "Just testing",
+  answersSmiles: ["CN5(CCN(CC4(=CC=C(C(NC3(=CC=C(C)C(NC2(=NC(C1(=CN=CC=C1))=CC=N2))=C3))=O)C=C4))CC5)"]
+},
 };
